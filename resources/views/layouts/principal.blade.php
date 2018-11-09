ab867d18a14569e03897c64603dd55af0c256e1c
rafael <rafaelborsatto58@gmail.com>
2018-11-09 15:32:27 -0200

    modificando front

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -69,13 +69,13 @@
                             <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                             
                             <!-- Light Logo icon -->
-                            <img src="{{ asset('images/pgmais-logo.png') }}" alt="homepage" class="light-logo" />
+                            <img src="{{ asset('images/logo-light-icon.png') }}" alt="homepage" class="light-logo" />
                         </b>
                         <!--End Logo icon -->
                         <!-- Logo text --><span>
                          
                          <!-- Light Logo text -->    
-                         <img src="{{ asset('images/pgmais-logo.png') }}" class="light-logo" alt="homepage" /></span> </a>
+                         <img src="{{ asset('images/logo-light-text.png ') }}" class="light-logo" alt="homepage" /></span> </a>
                 </div>
                 <!-- ============================================================== -->
                 <!-- End Logo -->
@@ -90,7 +90,6 @@
                         <!-- ============================================================== -->
                         <!-- Search -->
                         <!-- ============================================================== -->
-                        
                     </ul>
                     <!-- ============================================================== -->
                     <!-- User profile and search -->
@@ -106,10 +105,8 @@
                         </button>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             @csrf
-                        </form>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="{{ asset('images/users/1.jpg') }}" alt="user" class="profile-pic m-r-10" />Markarn Doe</a>
-                        </li>
+                        </form> 
+                        <li></li>
                     </ul>
                 </div>
             </nav>
