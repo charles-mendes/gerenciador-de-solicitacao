52f99d444693f6ee578fdf8bea5616224d63fe6d
Charles Mendes <charlesmendes9@gmail.com>
2018-11-19 08:29:27 -0200

    corrigindo seed

--- database/seeds/statusTableSeeder.php
+++ database/seeds/statusTableSeeder.php
@@ -11,21 +11,21 @@ class statusTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('situacao')->insert(['tipo_status' => 'Pendente']);
-        DB::table('situacao')->insert(['tipo_status' => 'Aprovado pelo Aprovador']);
-        DB::table('situacao')->insert(['tipo_status' => 'Aprovado pelo Comprador']);
-        DB::table('situacao')->insert(['tipo_status' => 'Aprovado pelo Administrador']);
-        DB::table('situacao')->insert(['tipo_status' => 'Aprovado pela Diretoria']);
-        DB::table('situacao')->insert(['tipo_status' => 'Reprovado pelo Aprovador']);
-        DB::table('situacao')->insert(['tipo_status' => 'Reprovado pelo Comprador']);
-        DB::table('situacao')->insert(['tipo_status' => 'Reprovado pela Diretoria']);
-        DB::table('situacao')->insert(['tipo_status' => 'Reprovado pelo Administrador']);
-        DB::table('situacao')->insert(['tipo_status' => 'Esperando Aprovação da diretoria']);
-        DB::table('situacao')->insert(['tipo_status' => 'Iniciou Cotação']);
-        DB::table('situacao')->insert(['tipo_status' => 'Finalizou Cotação']);
-        DB::table('situacao')->insert(['tipo_status' => 'Em processo de execução']);
-        DB::table('situacao')->insert(['tipo_status' => 'Finalizada']);
-        DB::table('situacao')->insert(['tipo_status' => 'Inativada']);
+        DB::table('status')->insert(['tipo_status' => 'Pendente']);
+        DB::table('status')->insert(['tipo_status' => 'Aprovado pelo Aprovador']);
+        DB::table('status')->insert(['tipo_status' => 'Aprovado pelo Comprador']);
+        DB::table('status')->insert(['tipo_status' => 'Aprovado pelo Administrador']);
+        DB::table('status')->insert(['tipo_status' => 'Aprovado pela Diretoria']);
+        DB::table('status')->insert(['tipo_status' => 'Reprovado pelo Aprovador']);
+        DB::table('status')->insert(['tipo_status' => 'Reprovado pelo Comprador']);
+        DB::table('status')->insert(['tipo_status' => 'Reprovado pela Diretoria']);
+        DB::table('status')->insert(['tipo_status' => 'Reprovado pelo Administrador']);
+        DB::table('status')->insert(['tipo_status' => 'Esperando Aprovação da diretoria']);
+        DB::table('status')->insert(['tipo_status' => 'Iniciou Cotação']);
+        DB::table('status')->insert(['tipo_status' => 'Finalizou Cotação']);
+        DB::table('status')->insert(['tipo_status' => 'Em processo de execução']);
+        DB::table('status')->insert(['tipo_status' => 'Finalizada']);
+        DB::table('status')->insert(['tipo_status' => 'Inativada']);
         
     }
 }
