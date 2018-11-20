7832f47c2d8190a2436e1a164790ccb09635eb50
Rafael Alcindo Borsatto <rafael.borsatto@pg.intra>
2018-11-20 11:01:13 -0200

    mudando o title da pagina de login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!--<title>{{ config('app.name', 'Laravel') }}</title> -->
+    <title>Gerenciador de Solicitação</title>
 
     <!-- Scripts -->
     <!-- <script src="{{-- asset('js/app.js') --}}" defer></script> -->
